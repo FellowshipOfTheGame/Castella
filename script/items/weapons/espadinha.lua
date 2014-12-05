436482de707d5ad356eeba2bf2d82064dc555e49
Rafael <ksuspec@gmail.com>
2014-12-05 21:41:37 -0200

    Battler filha da Actor; ambas classes escritas em mais detalhes; World inicializando os players;

---
+++ script/items/weapons/espadinha.lua
@@ -0,0 +1,11 @@
+--Arma = {nome, imgArq, custoBase, dano, fadiga, alcance}
+arma = {
+	nome = "espadinha"
+	imgArq = "items/espadinha.png" --Nome do arquivo de imagens (deve estar na pasta images)
+	custoBase = 10
+
+	--Atributos de batalha
+	dano = 10
+	fadiga = 25
+	alcance = 1
+}
